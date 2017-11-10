975186691cc78b9fd13ae8ae51146d03f54fb058
anlide <elfanlide@gmail.com>
2017-11-11 01:32:03 +0300

    Ability to stop tester

--- master.php
+++ master.php
@@ -8,16 +8,32 @@
 include 'num-cpus.php';
 $num_cpu = num_cpus();
 
-function execInBackground($cmd){
-  if (substr(php_uname(), 0, 7) == "Windows"){
-    $href = popen("start /B ". $cmd, "r");
-    usleep(10000);
-    pclose($href);
-  }else{
-    exec($cmd . " > /dev/null &");
+function execInBackground($cmd) {
+  return shell_exec(sprintf('%s > /dev/null 2>&1 & echo $!', $cmd));
+}
+
+$pids = [];
+for ($i = 0; $i < $num_cpu; $i++) {
+  $pids[$i] = execInBackground('php -q worker.php '.$i);
+}
+
+declare(ticks = 1);
+
+pcntl_signal(SIGINT, "signal_handler");
+
+function signal_handler($signal) {
+  global $pids;
+  switch($signal) {
+    case SIGINT:
+      print "Ctrl C\n";
+      foreach ($pids as $index => $pid) {
+        shell_exec('kill '.$pid);
+      }
+      exit;
+      break;
   }
 }
 
-for ($i = 0; $i < $num_cpu; $i++) {
-  execInBackground('php -q worker.php '.$i);
+while(true) {
+  usleep(10000);
 }
